9f68f95bd4625d956e48ad6ec40e4e60a16983fe
Tops <tops@Topss-iMac.local>
2016-03-14 15:38:06 +0530

    Initial Commit

---
+++ NSPredicateDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSPredicateDemo
+//
+//  Created by Tops on 3/14/16.
+//  Copyright (c) 2016 Tops. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
